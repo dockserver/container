554a0392ef57f0fb4ff531f883adef58bf995e8b
doob187 <60312740+doob187@users.noreply.github.com>
2022-08-05 00:51:03 +0200

    Update settings.php

--- apps/docker-uploader/root/var/www/html/settings.php
+++ apps/docker-uploader/root/var/www/html/settings.php
@@ -2,3 +2,5 @@
 
 define('JSON_DIRECTORY_COMPLETED', '/system/uploader/json/done');
 define('JSON_DIRECTORY_INPROGRESS', '/system/uploader/json/upload');
+define('TO_UPLOAD_FILE', '/system/uploader/logs/check.log');
+
